9645cd50eadfc93d327b02fc06f27e9bfb47f00f
Kit Yeung <kit.yeung@cs.hkbn.com.hk>
2024-03-08 18:14:33 +0800

    feat: context path config

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <Navigate replace to="/business-card-generator" />,
+    },
+    {
+        path: "/business-card-generator",
         element: <App />,
     }
 ]);
